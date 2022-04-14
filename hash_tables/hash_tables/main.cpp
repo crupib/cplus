ffd18b9db78c3931d7425ea1cfdead17d03a9874
bill <crupib@gmail.com>
2022-04-14 15:31:12 -0400

    new instead of malloc

--- hash_tables/hash_tables/main.cpp
+++ hash_tables/hash_tables/main.cpp
@@ -1,71 +1,95 @@
-//
-//  main.cpp
-//  hash_tables
-//
-//  Created by William Crupi on 4/14/22.
-//
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define SIZE 10
+#define SIZE 100000
 
-void create_snowflakes(int number_of_snow_flakes, int snowflakes[]) {
-    
+typedef struct snowflake_node {
+  int snowflake[6];
+  struct snowflake_node *next;
+} snowflake_node;
+
+int code(int snowflake[]) {
+  return (snowflake[0] + snowflake[1] + snowflake[2]
+          + snowflake[3] + snowflake[4] + snowflake[5]) % SIZE;
 }
-int identical_right(int snow1[], int snow2[], int start) {
-    int offset;
-    for (offset = 0; offset < 6; offset++) {
-        if (snow1[offset] != snow2[(start+offset)%6])
-              return 0;
-    }
-    return 1;
+
+int identical_right(int snow1[], int snow2[],
+                    int start) {
+  int offset, snow2_index;
+  for (offset =0; offset < 6; offset++) {
+    snow2_index = start + offset;
+    if (snow2_index >= 6)
+      snow2_index = snow2_index - 6;
+        if (snow1[offset] != snow2[snow2_index])
+      return 0;
+  }
+  return 1;
 }
 
 int identical_left(int snow1[], int snow2[], int start) {
-    int offset, snow2_index;
-    for (offset = 0; offset < 6; offset++){
-        snow2_index = start - offset;
-        if (snow2_index < 0)
-            snow2_index = snow2_index + 6;
-        if (snow1[offset] != snow2[snow2_index])
-            return 0;
-    }
-    return 1;
+  int offset, snow2_index;
+  for (offset =0; offset < 6; offset++) {
+    snow2_index = start - offset;
+    if (snow2_index < 0)
+      snow2_index = snow2_index + 6;
+    if (snow1[offset] != snow2[snow2_index])
+      return 0;
+  }
+  return 1;
 }
+
 int are_identical(int snow1[], int snow2[]) {
-    int start;
-    for (start = 0; start < 6; start++) {
-        if (identical_right(snow1,snow2,start))
-            return 1;
-        if (identical_left(snow1,snow2,start))
-            return 1;
-    }
-    return 0;
+  int start;
+  for (start = 0; start < 6; start++) {
+    if (identical_right(snow1, snow2, start))
+      return 1;
+    if (identical_left(snow1, snow2, start))
+      return 1;
+  }
+  return 0;
 }
 
-
-void identify_identical(int snowflakes[][6], int n) {
-  int i, j;
-  for (i = 0; i < n; i++) {
-   for (j = i+1; j < n; j++) {
-      if (are_identical(snowflakes[i],snowflakes[j])) {
-          printf("Twin snow flakes found.\n");
-        return;
+void identify_identical(snowflake_node *snowflakes[]) {
+  snowflake_node *node1, *node2;
+  int i;
+  for (i = 0; i < SIZE; i++) {
+    node1 = snowflakes[i];
+    while (node1 != NULL) {
+      node2 = node1->next;
+      while (node2 != NULL) {
+        if (are_identical(node1->snowflake, node2->snowflake)) {
+          printf("Twin snowflakes found.\n");
+          return;
+        }
+        node2 = node2->next;
       }
-   }
- }
- printf("No two snowflakes are alike\n");
- return;
+      node1 = node1->next;
+    }
+  }
+  printf("No two snowflakes are alike.\n");
 }
 
-static int snowflakes[SIZE][6];
-
-int main(int argc, const char * argv[]) {
-    int n, i, j;
-    printf("How many snowflakes ");
-    scanf("%d",&n);
-    for (i = 0; i < n; i++)
-        for (j=0; j<6;j++)
-           scanf("%d", &snowflakes[i][j]);
-    identify_identical(snowflakes, n);
-    return 0;
+int main(void) {
+ static snowflake_node *snowflakes[SIZE] = {NULL};
+  snowflake_node *snow;
+  int n, i, j, snowflake_code;
+  scanf("%d", &n);
+  for (i = 0; i < n; i++) {
+    //snow = (snowflake_node) malloc(sizeof(snowflake_node));
+    snow = new snowflake_node;
+    if (snow == NULL) {
+      fprintf(stderr, "malloc error\n");
+      exit(1);
+    }
+    for (j = 0; j < 6; j++)
+      scanf("%d", &snow->snowflake[j]);
+    snowflake_code = code(snow->snowflake);
+    snow->next = snowflakes[snowflake_code];
+    snowflakes[snowflake_code] = snow;
+      free(snow);
+  }
+  identify_identical(snowflakes);
+  //deallocate all malloc'd memory, if you want to be good
+  return 0;
 }
